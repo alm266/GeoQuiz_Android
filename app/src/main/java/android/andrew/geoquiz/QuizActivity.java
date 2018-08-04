e25a4005fc680edd8025384ba6ac67f7c3faff8e
Andrew Marshall <andrewleonmarshall@gmail.com>
2018-08-04 18:58:16 -0400

    1) Created previous button 2) Changed strings for prev and next buttons 3) Can go to next question by tapping question text

--- app/src/main/java/android/andrew/geoquiz/QuizActivity.java
+++ app/src/main/java/android/andrew/geoquiz/QuizActivity.java
@@ -11,6 +11,7 @@ public class QuizActivity extends Activity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mPrevButton;
     private Button mNextButton;
     private TextView mQuestionTextView;
 
@@ -29,6 +30,35 @@ public class QuizActivity extends Activity {
         mQuestionTextView.setText(question);
     }
 
+    private void nextCurrentIndex(){
+        mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+    }
+
+    private void prevCurrentIndex(){
+        int tempIndex = (mCurrentIndex - 1);
+        if(tempIndex < 0){
+            mCurrentIndex = mQuestionBank.length - 1;
+        }
+        else{
+            mCurrentIndex = tempIndex % mQuestionBank.length;
+        }
+
+    }
+
+    private void checkAnswer(boolean userPressedTrue){
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+
+        int messageResId = 0;
+
+        if(userPressedTrue == answerIsTrue){
+            messageResId = R.string.correct_toast;
+        }else{
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+                .show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,9 +71,7 @@ public class QuizActivity extends Activity {
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
@@ -51,19 +79,39 @@ public class QuizActivity extends Activity {
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
 
+
+        //previous question
+        mPrevButton = (Button)findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                prevCurrentIndex();
+                updateQuestion();
+            }
+        });
+
+        //next question
         mNextButton = (Button)findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                nextCurrentIndex();
                 updateQuestion();
             }
         });
+
+        //can tap on text to jump to next question as well
+        mQuestionTextView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                nextCurrentIndex();
+                updateQuestion();
+            }
+        });
+        updateQuestion();
     }
 }
